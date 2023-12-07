b0f7a9cb22ea9116f0e9ff74add2673d19565b73
Arkainas <kevin.vikander@gmail.com>
2023-12-07 15:14:07 +0100

    added customer table and its attributes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=idcgrupp4";
+
+await using var db = NpgsqlDataSource.Create(dbUri);
+
+await using (var cmd = db.CreateCommand("DROP TABLE IF EXISTS customer"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand("CREATE TABLE IF NOT EXISTS customer (email VARCHAR PRIMARY KEY, name VARCHAR, surname VARCHAR, phone_number VARCHAR, date_of_birth DATE)"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
